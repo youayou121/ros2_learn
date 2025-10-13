0f47d5493ea9392f2e8bd100fd346d90de9ea79e
youayou <1252759359@qq.com>
2025-10-13 11:00:41 +0800

    add cpp person node

---
+++ chapt2/chapt2_ws/src/demo_cpp_pkg/src/person_node.cpp
@@ -0,0 +1,33 @@
+#include <rclcpp/rclcpp.hpp>
+class PersonNode : public rclcpp::Node
+{
+private:
+    std::string name_;
+    int age_;
+
+public:
+    PersonNode(const std::string &node_name, const std::string &name, const int &age) : Node(node_name)
+    {
+        this->name_ = name;
+        this->age_ = age;
+        RCLCPP_INFO(this->get_logger(), "Hello, I am %s, %d years old.", this->name_.c_str(), this->age_);
+    }
+
+    void eat(const std::string &food)
+    {
+        RCLCPP_INFO(this->get_logger(), "%s is eating %s.", this->name_.c_str(), food.c_str());
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    std::shared_ptr<PersonNode> zhangsan_node = std::make_shared<PersonNode>("zhangsan_node", "Zhang San", 18);
+    std::shared_ptr<PersonNode> lisi_node = std::make_shared<PersonNode>("lisi_node", "Li Si", 18);
+    zhangsan_node->eat("apple");
+    lisi_node->eat("banana");
+    rclcpp::spin(lisi_node);
+    rclcpp::spin(zhangsan_node);
+    rclcpp::shutdown();
+    return 0;
+}
